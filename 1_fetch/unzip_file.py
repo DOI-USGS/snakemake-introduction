e7e823a890c607efb005f3bc63a87c873a3370fa
Andy McAliley <wmcaliley@usgs.gov>
2022-06-22 13:10:11 -0600

    Update unzip_file to match answer key

--- 1_fetch/unzip_file.py
+++ 1_fetch/unzip_file.py
@@ -1,17 +1,16 @@
 import zipfile
 import os
 
-def unzip_file(file_path, out_dir):
-    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+def unzip_file(zip_file_path, out_dir):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(out_dir)
 
-def main(file_path, out_dir):
+def main(zip_file_path, out_dir):
     if not os.path.exists(out_dir):
 	    os.makedirs(out_dir)
-    unzip_file(file_path, out_dir)
+    unzip_file(zip_file_path, out_dir)
 
 if __name__ == '__main__':
-    sb_file = "pgdl_predictions_04_N45.50-48.00_W92.00-93.00.zip"
-    out_dir = "1_fetch/out/"
-    file_path = os.path.join(out_dir, sb_file)
-    main(file_path, out_dir)
+    zip_file_path = "1_fetch/tmp/pgdl_predictions_04_N45.50-48.00_W92.00-93.00.zip"
+    out_dir = "1_fetch/out/pgdl/"
+    main(zip_file_path, out_dir)
